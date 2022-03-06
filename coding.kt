a9474d03edba341cbc841d2eb9acd1e4c8da7d45
Hadi Albinsaad <hadi.albinsaad@gmail.com>
2022-03-06 13:41:52 +0300

    Update README.md

---
+++ coding.kt
@@ -0,0 +1,41 @@
+
+
+fun main(){
+    // Q1 Create a function that receives an integers array and returns the number that appeared only once.
+// Q1  input (2,4,6,4,9,6,2) ., output (9)
+    val myaray= arrayOf(2,4,6,4,9,6,2)
+    println(myaray[4])
+}
+// Q1  input(1,1,2) , output(2)
+fun main (){
+    var myarray1 = arrayOf(1,1,2)
+    println(myarray1[2])
+}
+
+//Q1 input (2,2,1) output(1)
+
+fun main (){
+    var myarray2 = arrayOf(2,2,1)
+    println(myarray2[2])
+}
+
+
+// Q2 : Create a function that receives a string then it converts uppercase letters into lowercase and vice versa. The function then should print the converted value.
+//input Hello  , output hELLO
+//Hello World	hELLO wORLD
+//My name is Ali	mY NAME IS aLI
+//sHroog	ShROOG
+
+fun main (){
+    val hello =MutableMap ("Hello" in "hELLO")
+    println(hello)
+
+}
+
+fun main (){
+    val hello =MutableMap ("Hello World" in "hELLO wORLD")
+    println(hello)
+
+
+
+
